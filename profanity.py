1a29cd16f637efed32a3bf9b15abe23d87f93838
anushikab <anushikab@gmail.com>
2017-05-21 23:10:07 -0700

    parent
    parent error

--- profanity.py
+++ profanity.py
@@ -1,3 +1,6 @@
 def read_text():
+    quotes = open("C:/Users/anushikabaranage/Desktop/movie_quotes.txt")
+    content_of_life = quotes.read()
     print(content_of_files)
+    quotes.close
     read_text()
